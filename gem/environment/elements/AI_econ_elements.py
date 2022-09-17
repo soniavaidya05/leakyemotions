ff7a189c1ae6306b85e9c9e02691fa7f41c0d8b8
Wil Cunningham <39409282+wacunn@users.noreply.github.com>
2022-09-16 20:27:29 -0400

    Added AI_econ game

---
+++ gem/environment/elements/AI_econ_elements.py
@@ -0,0 +1,232 @@
+from gem.environment.elements.element import Element
+import random
+from collections import deque
+import numpy as np
+import torch
+from gem.environment.elements.element import Wall
+
+
+class Wood(Element):
+
+    kind = "wood"  # class variable shared by all instances
+
+    def __init__(self):
+        super().__init__()
+        self.health = 1  # for the wood, whether it has been mined or not
+        self.appearence = (160, 82, 45)  # wood is brown
+        self.vision = 1  # gems can see one radius around them
+        self.policy = "NA"  # gems do not do anything
+        self.value = 0  # the value of this wood
+        self.reward = 0  # how much reward this wood has found (will remain 0)
+        self.static = 1  # whether the object gets to take actions or not
+        self.passable = 1  # whether the object blocks movement
+        self.trainable = 0  # whether there is a network to be optimized
+        self.has_transitions = False
+
+
+class Stone(Element):
+
+    kind = "stone"  # class variable shared by all instances
+
+    def __init__(self):
+        super().__init__()
+        self.health = 1  # for the stone, whether it has been mined or not
+        self.appearence = (136, 140, 141)  # stone is grey
+        self.vision = 1  # gems can see one radius around them
+        self.policy = "NA"  # gems do not do anything
+        self.value = 10  # the value of this stone
+        self.reward = 0  # how much reward this wood has found (will remain 0)
+        self.static = 1  # whether the object gets to take actions or not
+        self.passable = 1  # whether the object blocks movement
+        self.trainable = 0  # whether there is a network to be optimized
+        self.has_transitions = False
+
+
+class House(Element):
+
+    kind = "house"  # class variable shared by all instances
+
+    def __init__(self):
+        super().__init__()
+        self.health = 1  # for the stone, whether it has been mined or not
+        self.appearence = (220, 216, 199)  # houses are tan
+        self.vision = 1  # gems can see one radius around them
+        self.policy = "NA"  # gems do not do anything
+        self.value = 10  # the value of this stone
+        self.reward = 0  # how much reward this wood has found (will remain 0)
+        self.static = 1  # whether the object gets to take actions or not
+        self.passable = 1  # whether the object blocks movement
+        self.trainable = 0  # whether there is a network to be optimized
+        self.has_transitions = False
+
+
+class EmptyObject(Element):
+
+    kind = "empty"  # class variable shared by all instances
+
+    def __init__(self):
+        self.health = 0  # empty stuff is basically empty
+        self.appearence = [0.0, 0.0, 0.0]  # empty is well, blank
+        self.vision = 1  # empty stuff is basically empty
+        self.policy = "NA"  # empty stuff is basically empty
+        self.value = 0  # empty stuff is basically empty
+        self.reward = 0  # empty stuff is basically empty
+        self.static = 1  # whether the object gets to take actions or not
+        self.passable = 1  # whether the object blocks movement
+        self.trainable = 0  # whether there is a network to be optimized
+        self.has_transitions = False
+        self.stone = 0
+        self.wood = 0
+
+    def transition(self, world, models, action, location):
+        generate_value = random.random.choice([0, 1, 2], p=[0.8, 0.1, 0.1])
+        if generate_value == 0:
+            world[location] = Wood()
+        elif generate_value == 1:
+            world[location] = Stone()
+        else:
+            world[location] = House()
+        reward = 0
+        next_state = 0
+        done = 0
+        new_loc = location
+        return world, reward, next_state, done, new_loc
+
+
+class Agent:
+
+    kind = "agent"  # class variable shared by all instances
+
+    def __init__(self, model):
+        self.health = 10  # for the agents, this is how hungry they are
+        self.appearence = [0.0, 0.0, 255.0]  # agents are blue
+        self.vision = 4  # agents can see three radius around them
+        self.policy = model  # agent model here. need to add a tad that tells the learning somewhere that it is DQN
+        self.value = 0  # agents have no value
+        self.reward = 0  # how much reward this agent has collected
+        self.static = 0  # whether the object gets to take actions or not
+        self.passable = 0  # whether the object blocks movement
+        self.trainable = 1  # whether there is a network to be optimized
+        self.replay = deque([], maxlen=100)  # we should read in these maxlens
+        self.has_transitions = True
+        self.stone = 0
+        self.wood = 0
+
+    def init_replay(self, numberMemories):
+        """
+        Fills in blank images for the LSTM before game play.
+        Impicitly defines the number of sequences that the LSTM will be trained on.
+        """
+        pov_size = 9
+        image = torch.zeros(1, numberMemories, 3, pov_size, pov_size).float()
+        exp = (image, 0, 0, image, 0)
+        self.replay.append(exp)
+
+    def transition(self, world, models, action, location):
+        i, j, k = location
+        done = 0
+        new_loc = location
+
+        attempted_locaton_1 = i
+        attempted_locaton_2 = j
+        attempted_locaton_3 = k
+
+        reward = 0
+
+        if action == 0:
+            attempted_locaton_1 = i - 1
+            attempted_locaton_2 = j
+
+        if action == 1:
+            attempted_locaton_1 = i + 1
+            attempted_locaton_2 = j
+
+        if action == 2:
+            attempted_locaton_1 = i
+            attempted_locaton_2 = j - 1
+
+        if action == 3:
+            attempted_locaton_1 = i
+            attempted_locaton_2 = j + 1
+
+        attempted_location = (
+            attempted_locaton_1,
+            attempted_locaton_2,
+            attempted_locaton_3,
+        )
+
+        if action < 4:
+            if isinstance(world[attempted_location], Wall):
+                reward = -0.1
+
+            if isinstance(world[attempted_location], House):
+                reward = -0.1
+
+            if isinstance(world[attempted_location], Wood):
+                reward = 1
+                self.wood += 1
+                world[attempted_location] = self
+                world[location] = EmptyObject()
+                new_loc = attempted_location
+
+            if isinstance(world[attempted_location], Stone):
+                reward = 1
+                self.stone += 1
+                world[attempted_location] = self
+                world[location] = EmptyObject()
+                new_loc = attempted_location
+
+        if action == 5:
+            move_to = np.array([1, 1, 1, 1])
+            if world[i + 1, j, k] != EmptyObject():
+                move_to[0] = 0
+                attempted_locaton_1 = i + 1
+                attempted_locaton_2 = j
+            if world[i - 1, j, k] != EmptyObject():
+                move_to[1] = 0
+                attempted_locaton_1 = i - 1
+                attempted_locaton_2 = j
+            if world[i, j - 1, k] != EmptyObject():
+                move_to[2] = 0
+                attempted_locaton_1 = i
+                attempted_locaton_2 = j - 1
+            if world[i, j + 1, k] != EmptyObject():
+                move_to[3] = 0
+                attempted_locaton_1 = i
+                attempted_locaton_2 = j + 1
+            attempted_location = (
+                attempted_locaton_1,
+                attempted_locaton_2,
+                attempted_locaton_3,
+            )
+            if self.stone > 0 and self.wood > 0 and sum(move_to) > 0:
+                reward = 10
+                self.stone -= 1
+                self.wood -= 1
+                world[attempted_location] = self
+                world[location] = EmptyObject()
+                new_loc = attempted_location
+
+        next_state = models[self.policy].pov(world, new_loc, self)
+        self.reward += reward
+
+        return world, reward, next_state, done, new_loc
+
+        if world[attempted_locaton_1, attempted_locaton_2, 0].passable == 1:
+            world[i, j, 0] = EmptyObject()
+            reward = world[attempted_locaton_1, attempted_locaton_2, 0].value
+            world[attempted_locaton_1, attempted_locaton_2, 0] = self
+            new_locaton_1 = attempted_locaton_1
+            new_locaton_2 = attempted_locaton_2
+
+        else:
+            if isinstance(
+                world[attempted_locaton_1, attempted_locaton_2, 0], Wall
+            ):  # Replacing comparison with string 'kind'
+                reward = -0.1
+        new_loc = (new_locaton_1, new_locaton_2, 0)
+
+        next_state = models[self.policy].pov(world, new_loc, self)
+        self.reward += reward
+
+        return world, reward, next_state, done, new_loc
---
+++ gem/gemworld/AI_econ_world.py
@@ -0,0 +1,199 @@
+from gem.environment.elements.AI_econ_elements import (
+    Agent,
+    Wood,
+    Stone,
+    House,
+    EmptyObject,
+)
+from gem.environment.elements.element import Wall
+import numpy as np
+from astropy.visualization import make_lupton_rgb
+import matplotlib.pyplot as plt
+from models.perception import agent_visualfield
+import random
+
+from utils import (
+    find_moveables,
+)
+
+
+class AI_Econ:
+    def __init__(
+        self,
+        height=30,
+        width=30,
+        layers=1,
+        defaultObject=EmptyObject(),
+        wood1p=0.04,
+        stone1p=0.04,
+    ):
+        self.wood1p = wood1p
+        self.stone1p = stone1p
+        self.height = height
+        self.width = width
+        self.layers = layers
+        self.defaultObject = defaultObject
+        self.create_world(self.height, self.width, self.layers)
+        self.init_elements()
+        self.populate(self.wood1p, self.stone1p)
+        self.insert_walls(self.height, self.width)
+
+    def create_world(self, height=30, width=30, layers=1):
+        """
+        Creates a world of the specified size with a default object
+        """
+        self.world = np.full((height, width, layers), self.defaultObject)
+
+    def reset_env(self, height=30, width=30, layers=1, wood1p=0.04, stone1p=0.04):
+        """
+        Resets the environment and repopulates it
+        """
+        self.create_world(height, width, layers)
+        self.populate(wood1p, stone1p)
+        self.insert_walls(height, width)
+
+    def plot(self, layer):  # is this defined in the master?
+        """
+        Creates an RGB image of the whole world
+        """
+        image_r = np.random.random((self.world.shape[0], self.world.shape[1]))
+        image_g = np.random.random((self.world.shape[0], self.world.shape[1]))
+        image_b = np.random.random((self.world.shape[0], self.world.shape[1]))
+
+        for i in range(self.world.shape[0]):
+            for j in range(self.world.shape[1]):
+                image_r[i, j] = self.world[i, j, layer].appearence[0]
+                image_g[i, j] = self.world[i, j, layer].appearence[1]
+                image_b[i, j] = self.world[i, j, layer].appearence[2]
+
+        image = make_lupton_rgb(image_r, image_g, image_b, stretch=0.5)
+        return image
+
+    def init_elements(self):
+        """
+        Creates objects that survive from game to game
+        """
+        self.emptyObject = EmptyObject()
+        self.walls = Wall()
+
+    def game_test(self, layer=0):
+        """
+        Prints one frame to check game instance parameters
+        """
+        image = self.plot(layer)
+
+        moveList = []
+        for i in range(self.world.shape[0]):
+            for j in range(self.world.shape[1]):
+                if self.world[i, j, 0].static == 0:
+                    moveList.append([i, j])
+
+        img = agent_visualfield(self.world, (moveList[0][0], moveList[0][1]), k=4)
+
+        plt.subplot(1, 2, 1)
+        plt.imshow(image)
+        plt.subplot(1, 2, 2)
+        plt.imshow(img)
+        plt.show()
+
+    def populate(self, wood1p=0.04, stone1p=0.04):
+        """
+        Populates the game board with elements
+        TODO: test whether the probabilites above are working
+        """
+
+        for i in range(self.world.shape[0]):
+            for j in range(self.world.shape[1]):
+                obj = np.random.choice(
+                    [0, 1, 2],
+                    p=[
+                        wood1p,
+                        stone1p,
+                        1 - wood1p - stone1p,
+                    ],
+                )
+                if obj == 0:
+                    self.world[i, j, 0] = Wood()
+                if obj == 1:
+                    self.world[i, j, 0] = Stone()
+
+        self.world[
+            round(self.world.shape[0] / 2), round(self.world.shape[1] / 2), 0
+        ] = Agent(0)
+        self.world[
+            round(self.world.shape[0] / 2) + 1,
+            round(self.world.shape[1] / 2) - 1,
+            0,
+        ] = Agent(0)
+
+    def insert_walls(self, height, width):
+        """
+        Inserts walls into the world.
+        Assumes that the world is square - fixme.
+        """
+        for i in range(height):
+            self.world[0, i, 0] = Wall()
+            self.world[height - 1, i, 0] = Wall()
+            self.world[i, 0, 0] = Wall()
+            self.world[i, height - 1, 0] = Wall()
+
+        # this is a hack to get to look like AI economist
+        for i in range(8):
+            self.world[14, i, 0] = Wall()
+            self.world[i, 14, 0] = Wall()
+        for i in range(8):
+            self.world[14, height - i - 1, 0] = Wall()
+            self.world[height - i - 1, 14, 0] = Wall()
+
+    def step(self, models, loc, epsilon=0.85):
+        """
+        This is an example script for an alternative step function
+        It does not account for the fact that an agent can die before
+        it's next turn in the moveList. If that can be solved, this
+        may be preferable to the above function as it is more like openAI gym
+
+        The solution may come from the agent.died() function if we can get that to work
+
+        location = (i, j, 0)
+
+        Uasge:
+            for i, j, k = agents
+                location = (i, j, k)
+                state, action, reward, next_state, done, additional_output = env.stepSingle(models, (0, 0, 0), epsilon)
+                env.world[0, 0, 0].updateMemory(state, action, reward, next_state, done, additional_output)
+            env.WorldUpdate()
+
+        """
+        holdObject = self.world[loc]
+
+        if holdObject.static != 1:
+            """
+            This is where the agent will make a decision
+            If done this way, the pov statement may be about to be part of the action
+            Since they are both part of the same class
+
+            if going for this, the pov statement needs to know about location rather than separate
+            i and j variables
+            """
+            state = models[holdObject.policy].pov(self.world, loc, holdObject)
+            action = models[holdObject.policy].take_action([state, epsilon])
+
+        if holdObject.has_transitions == True:
+            """
+            Updates the world given an action
+            TODO: does this need self.world in here, or can it be figured out by passing self?
+            """
+            (
+                self.world,
+                reward,
+                next_state,
+                done,
+                new_loc,
+            ) = holdObject.transition(self.world, models, action, loc)
+        else:
+            reward = 0
+            next_state = state
+
+        additional_output = []
+
+        return state, action, reward, next_state, done, new_loc, additional_output
