bf206074143bbb6ca6d5bfde29eaf7c51aee64b5
Wil Cunningham <39409282+wacunn@users.noreply.github.com>
2022-11-12 22:09:37 -0500

    Update main.py

--- examples/taxi_cab_rainbow/main.py
+++ examples/taxi_cab_rainbow/main.py
@@ -42,7 +42,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # if torch.backends.mps.is_available():
 #    device = torch.device("mps")
 
-# device = "cpu"
+# device = "mps"
 print(device)
 
 SEED = 1  # Seed for replicating training runs
