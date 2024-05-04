13f56778c0cc5b96af91a890588c40eb2c6e56a2
rgelpi <46760714+rgelpi@users.noreply.github.com>
2024-05-04 12:07:09 -0400

    push old tweaks

--- gem/models/humanPlayer.py
+++ gem/models/humanPlayer.py
@@ -5,27 +5,42 @@ from IPython.display import clear_output
 class ModelHumanPlayer:
 
     def __init__(self, action_space, memory_size):
-        self.action_space = action_space
+        self.action_space = np.arange(action_space)
         self.memory_size = memory_size
+        self.num_frames = memory_size
+        self.show = False
 
     def take_action(self, state):
         
-        clear_output(wait = True)
-        for i in range(self.memory_size):
-            frame = state[:, i, :, :, :].squeeze().permute(1, 2, 0).numpy().astype(np.uint8)
-            plt.subplot(1, self.memory_size, i+1)
-            plt.imshow(frame)
-        plt.show()
+        if self.show:
+            clear_output(wait = True)
+            for i in range(self.memory_size):
+                frame = state[:, i, :, :, :].squeeze().permute(1, 2, 0).numpy().astype(np.uint8)
+                plt.subplot(1, self.memory_size, i+1)
+                plt.imshow(frame)
+            plt.show()
         
         done = 0
         while done == 0:
-            action = int(input("Select Action: "))
-            if action in self.action_space:
-                done = 1
+            action_ = input("Select Action: ")
+            if action_ in ["w", "a", "s", "d"]:
+                if action_ == "w":
+                    action = 0
+                elif action_ == "s":
+                    action = 1
+                elif action_ == "a":
+                    action = 2
+                elif action_ == "d":
+                    action = 3
+            elif action_ in ["0", "1", "2", "3"]:
+                action = int(action_)
             else:
                 print("Please try again. Possible actions are below.")
-                print(self.actionSpace)
+                print(self.action_space)
                 # we can have iinputType above also be joystick, or other controller
+            if action is not None:
+                if action in self.action_space:
+                    done = 1
 
         return action
 
