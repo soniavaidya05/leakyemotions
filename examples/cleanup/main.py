bd339d122948ee640e6e41c8c5388bd916a33037
rgelpi <46760714+rgelpi@users.noreply.github.com>
2024-11-18 10:58:48 -0500

    Update main.py

--- examples/cleanup/main.py
+++ examples/cleanup/main.py
@@ -37,8 +37,13 @@ def main():
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("--config", "-c", type=str, help="Path to configuration file", default="./configs/config.yaml")
-    parser.add_argument("--load-weights", "-l", type=str, help="Path to pretrained model.")
+    parser.add_argument("--load-weights", "-l", type=str, help="Path to pretrained model.", default="")
     parser.add_argument("--verbose", "-v", action="count", default=0)
     args = parser.parse_args()
     cfg = load_config(args)
+    run(
+        cfg,
+        load_weights=args.load_weights,
+        save_weights=os.path.abspath(f'./models/checkpoints/{cfg.model.iqn.type}_{datetime.now().strftime("%Y%m%d-%H%m%s")}.pkl')
+    )
 
