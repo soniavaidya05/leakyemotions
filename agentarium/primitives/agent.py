a1866b218b83d550e83cc4d4dd4ed8237095c3cf
YBthree <144300516+bing-j@users.noreply.github.com>
2024-11-23 22:37:59 -0500

    added documentation for Agent

--- agentarium/primitives/agent.py
+++ agentarium/primitives/agent.py
@@ -1,16 +1,32 @@
-
 import abc
+from typing import Any
 
-import torch
 import numpy as np
+import torch
 
-from agentarium.primitives.environment import GridworldEnv, Entity
 from agentarium.config import Cfg
+from agentarium.primitives.environment import Entity, GridworldEnv
 
 
 class Agent(Entity):
-    """Abstract agent class."""
-    def __init__(self, cfg: Cfg, appearance, model, action_space, location = None):
+    r"""
+    An abstract class for agents, a special type of entities. Note that this is a subclass of Entity.
+
+    Attributes:
+        - :attr:`cfg` - The configuration to use for this agent.
+        - :attr:`model` - The model that this agent uses.
+        - :attr:`action_space` - The set of actions that the agent is able to take.
+
+    Attributes that override parent (Entity)'s default values:
+        - :attr:`vision` - set at time of initialization based on the cfg provided, instead of defaulting to None.
+        - :attr:`has_transitions` - Defaults to True instead of False.
+    """
+
+    cfg: Cfg
+    model: Any
+    action_space: Any
+
+    def __init__(self, cfg: Cfg, appearance, model, action_space, location=None):
 
         # initializations based on parameters
         self.cfg = cfg
@@ -28,11 +44,11 @@ class Agent(Entity):
     def act(self, action) -> tuple[int, ...]:
         """Act on the environment.
 
-        Params:
+        Args:
             action: an element from this agent's action space indicating the action to take.
 
-        Return:
-            tuple[int, ...] A location tuple indicating the updated location of the agent.
+        Returns:
+            tuple[int, ...]: A location tuple indicating the updated location of the agent.
         """
         pass
 
@@ -40,6 +56,12 @@ class Agent(Entity):
     def pov(self, env: GridworldEnv) -> torch.Tensor:
         """
         Defines the agent's observation function.
+
+        Args:
+            env (GridworldEnv): the environment that this agent is observing.
+
+        Returns:
+            torch.Tensor: the observed information.
         """
         pass
 
@@ -47,14 +69,27 @@ class Agent(Entity):
     def transition(self, env: GridworldEnv, state, action) -> torch.Tensor:
         """
         Changes the environment based on action taken by the agent.
+
+        Args:
+            env (GridworldEnv): the environment that this agent is acting in.
+            state: the current state.
+            action: an element from this agent's action space indicating the action taken.
+
+        Returns:
+            torch.Tensor: the result of the transition.
         """
         pass
 
     # TODO: leave as implemented or change to abstract?
-    def add_memory(self, state: np.ndarray, action: int, reward: float, done: bool) -> None:
+    def add_memory(
+        self, state: np.ndarray, action: int, reward: float, done: bool
+    ) -> None:
         """Add an experience to the memory."""
         self.model.memory.add(state, action, reward, done)
 
     @abc.abstractmethod
     def reset(self) -> None:
+        """
+        Reset the agent (and its memory).
+        """
         pass
--- docs/source/reference/primitives/agent.md
+++ docs/source/reference/primitives/agent.md
@@ -1,1 +1,32 @@
 # Agent
+
+```{eval-rst}
+.. autoclass:: agentarium.primitives.Agent
+```
+
+## Methods
+Note that the following methods must be implemented in any implementation of this abstract class.
+
+```{eval-rst}
+.. automethod:: agentarium.primitives.Agent.act
+.. automethod:: agentarium.primitives.Agent.pov
+.. automethod:: agentarium.primitives.Agent.transition
+.. automethod:: agentarium.primitives.Agent.reset
+```
+TODO: get an answer on add_memory()
+
+## Attributes
+```{eval-rst}
+        
+.. autoattribute:: agentarium.primitives.Agent.cfg
+
+    The configuration to use for this agent.
+    
+.. autoattribute:: agentarium.primitives.Agent.model
+
+    The model that this agent uses.
+    
+.. autoattribute:: agentarium.primitives.Agent.action_space
+
+    The set of actions that the agent is able to take.
+```
