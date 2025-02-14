2b9b5a36dddd2ca60ca2b588bddcc6eb03a75ad2
YBthree <144300516+bing-j@users.noreply.github.com>
2025-02-14 16:35:52 -0500

    added main.py for ai econ (not tested). should be able to run an experiment assuming config file is filled

--- examples/ai_econ/main.py
+++ examples/ai_econ/main.py
@@ -1,87 +1,45 @@
 # general imports
 import argparse
-import torch
 
 # agentarium imports
-from agentarium.models.pytorch import PyTorchIQN
-from agentarium.observation.observation import ObservationSpec
-from agentarium.config import Cfg, load_config
+from agentarium.config import load_config
 from agentarium.utils.visualization import (animate, image_from_array,
                                             visual_field_sprite)
 # imports from our example
-from examples.ai_econ.agents import Seller
 from examples.ai_econ.env import EconEnv
-
-# experiment parameters
-EPOCHS = 1000
-MAX_TURNS = 100
-EPSILON_DECAY = 0.0001
-ENTITY_LIST = ["EmptyEntity", "Wall", "Land", "WoodNode", "StoneNode", "Seller"]
-RECORD_PERIOD = 50  # how many epochs in each data recording period
+from examples.ai_econ.utils import create_agents, create_models
 
 
-def setup() -> EconEnv:
+def setup(cfg) -> EconEnv:
     """Set up all the whole environment and everything within."""
-    # object configurations
-    world_height = 21
-    world_width = 21
-    gem_value = 1
-    spawn_prob = 0.0001
-    agent_vision_radius = 2
-    cfg: Cfg = load_config(argparse.Namespace(config="./configs/config.yaml"))
 
     # make the agents
-    agent_num = 5
-    agents = []
-    for _ in range(agent_num):
-        observation_spec = ObservationSpec(ENTITY_LIST, vision_radius=agent_vision_radius)
-
-        model = PyTorchIQN(
-            # the agent can see r blocks on each side, so the size of the observation is (2r+1) * (2r+1)
-            input_size=(len(ENTITY_LIST), 2 * agent_vision_radius + 1, 2 * agent_vision_radius + 1),
-            action_space=5,
-            layer_size=250,
-            epsilon=0.7,
-            device="cpu",
-            seed=torch.random.seed(),
-            num_frames=5,
-            n_step=3,
-            sync_freq=200,
-            model_update_freq=4,
-            BATCH_SIZE=64,
-            memory_size=1024,
-            LR=0.00025,
-            TAU=0.001,
-            GAMMA=0.99,
-            N=12,
-        )
-
-        agents.append(Seller(
-            cfg=cfg,
-            appearance=[],
-            is_woodcutter=True,
-            is_majority=True,
-            observation_spec=observation_spec, 
-            model=model))
+    woodcutter_models, stonecutter_models, market_models = create_models(cfg)
+    woodcutters, stonecutters, markets = create_agents(
+        cfg, woodcutter_models, stonecutter_models, market_models
+    )
 
     # make the environment
-    env = EconEnv(world_height, world_width, gem_value, spawn_prob, MAX_TURNS, agents)
-    return env
+    return EconEnv(cfg, woodcutters, stonecutters, markets)
 
 
-def run(env: EconEnv):
+def run(env: EconEnv, cfg):
     """Run the experiment."""
     imgs = []
-    total_score = 0
-    total_loss = 0
-    for epoch in range(EPOCHS + 1):
+    total_seller_score = 0
+    total_seller_loss = 0
+    total_buyer_loss = 0
+    for epoch in range(cfg.experiment.epochs + 1):
         # Reset the environment at the start of each epoch
         env.reset()
-        for agent in env.agents:
-            agent.model.start_epoch_action(**locals())
+        for i in cfg.agent.seller.num:
+            env.woodcutters[i].model.start_epoch_action(**locals())
+            env.stonecutters[i].model.start_epoch_action(**locals())
+        for i in cfg.agent.buyer.num:
+            env.markets[i].model.start_epoch_action(**locals())
 
         while not env.turn >= env.max_turns:
-            if epoch % RECORD_PERIOD == 0:
+            if epoch % cfg.env.record_period == 0:
                 full_sprite = visual_field_sprite(env)
                 imgs.append(image_from_array(full_sprite))
 
@@ -89,16 +47,19 @@ def run(env: EconEnv):
 
         # At the end of each epoch, train as long as the batch size is large enough.
         if epoch > 10:
-            for agent in env.agents:
-                loss = agent.model.train_step()
-                total_loss += loss
+            for i in cfg.agent.seller.num:
+                total_seller_loss += env.woodcutters[i].model.train_step()
+                total_seller_loss += env.stonecutters[i].model.train_step()
+            for i in cfg.agent.buyer.num:
+                total_buyer_loss += env.markets[i].model.train_step()
 
-        total_score += env.game_score
+        total_seller_score += env.seller_score
+        current_seller_epsilon = env.woodcutters[0].model.epsilon
 
-        if epoch % RECORD_PERIOD == 0:
-            avg_score = total_score / RECORD_PERIOD
+        if epoch % cfg.env.record_period == 0:
+            avg_seller_score = total_seller_score / cfg.env.record_period
             print(
-                f"Epoch: {epoch}; Epsilon: {env.agents[0].model.epsilon}; Losses this period: {total_loss}; Avg. score this period: {avg_score}"
+                f"Epoch: {epoch}; Epsilon: {current_seller_epsilon}; Losses this period: {total_seller_loss}; Avg. score this period: {avg_seller_score}"
             )
             animate(imgs, f"econ_epoch{epoch}", "./data/")
             # reset the data
@@ -107,11 +68,13 @@ def run(env: EconEnv):
             total_loss = 0
 
         # update epsilon
-        for agent in env.agents:
-            new_epsilon = agent.model.epsilon - EPSILON_DECAY
-            agent.model.epsilon = max(new_epsilon, 0.01)
+        for i in cfg.agent.seller.num:
+            new_epsilon = current_seller_epsilon - cfg.env.epsilon_decay
+            env.woodcutters[i].model.epsilon = max(new_epsilon, 0.01)
+            env.stonecutters[i].model.epsilon = max(new_epsilon, 0.01)
 
 
 if __name__ == "__main__":
-    env = setup()
-    run(env)
+    config = load_config(argparse.Namespace(config="./configs/config.yaml"))
+    environment = setup(config)
+    run(environment, config)
