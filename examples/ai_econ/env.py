f404a838e7c472f8a3fc0bf7cb56302aad2af5be
YBthree <144300516+bing-j@users.noreply.github.com>
2025-02-19 20:49:55 -0500

    added back all ai_econ files on the restructure branch.

---
+++ examples/ai_econ/env.py
@@ -0,0 +1,130 @@
+import random
+
+import numpy as np
+
+from agentarium.environments import GridworldEnv
+from examples.ai_econ.entities import (EmptyEntity, Land, StoneNode, Wall,
+                                       WoodNode)
+
+
+class EconEnv(GridworldEnv):
+    """
+    AI Economist environment.
+    """
+
+    def __init__(self, cfg, woodcutters, stonecutters, markets):
+        layers = 3
+        default_entity = EmptyEntity()
+        super().__init__(cfg.env.height, cfg.env.width, layers, default_entity)
+
+        self.cfg = cfg
+        self.woodcutters = woodcutters
+        self.stonecutters = stonecutters
+        self.markets = markets
+
+        # TODO: based on the size of the environment, have a hard limit on the number of agents
+
+        self.max_turns = cfg.experiment.max_turns
+        self.seller_score = 0
+        self.buyer_score = 0
+
+        self.populate()
+
+    def populate(self):
+        """
+        Populate the treasurehunt world by creating walls, then randomly spawning 1 gem and 1 agent.
+        Note that every space is already filled with EmptyEntity as part of super().__init__().
+
+        Note: work in progress to make this change with the size of the world!
+        For now, the environment is somewhat hard coded and only works with height=51 and width=51.
+        """
+        for index in np.ndindex(self.world.shape):
+            y, x, z = index
+
+            # walls
+            if y in [0, self.height - 1] or x in [0, self.width - 1]:
+                # Add walls around the edge of the world (when indices are first or last)
+                self.add(index, Wall())
+            if (x == 15 or x == 35) and (y in range(15, 20) or y in range(31, 36)):
+                # Add top & bottom walls around the market area
+                self.add(index, Wall())
+            if (y == 15 or y == 35) and (x in range(15, 20) or x in range(31, 36)):
+                # Add left & right walls around the market area
+                self.add(index, Wall())
+
+            if z == 0:
+                self.add(index, Land())
+
+            # resource nodes, which are on the bottom layer
+            if z == 1:
+                # wood nodes
+                if x in range(1, 15) and y in range(1, 15):
+                    self.add(index, WoodNode(self.cfg))
+                elif x in range(36, 50) and y in range(36, 50):
+                    self.add(index, WoodNode(self.cfg))
+                # stone nodes
+                elif x in range(1, 15) and y in range(36, 50):
+                    self.add(index, StoneNode(self.cfg))
+                elif x in range(36, 50) and y in range(1, 15):
+                    self.add(index, StoneNode(self.cfg))
+                # land
+                else:
+                    self.add(index, EmptyEntity())
+
+        # finished filling in entities; spawn agents in a separate method cause this one is getting long
+        self.place_agents()
+
+    def place_agents(self):
+        """
+        Places the agents into the environment.
+        """
+        north_spawn_area = []
+        south_spawn_area = []
+        east_spawn_area = []
+        west_spawn_area = []
+        for y, x in np.ndindex(self.height, self.width):
+            if x in range(20, 31):
+                if y in range(1, 15):
+                    north_spawn_area.append((y, x, 2))
+                if y in range(36, 50):
+                    south_spawn_area.append((y, x, 2))
+            if y in range(20, 31):
+                if x in range(1, 15):
+                    west_spawn_area.append((y, x, 2))
+                if x in range(36, 50):
+                    east_spawn_area.append((y, x, 2))
+
+        # woodcutters: north area & south area
+        woodcutters_spawn_locations = random.sample(
+            north_spawn_area, self.cfg.agent.seller.num // 2
+        ) + random.sample(south_spawn_area, self.cfg.agent.seller.num // 2)
+        random.shuffle(woodcutters_spawn_locations)
+        # stonecutters: east area & west area
+        stonecutters_spawn_locations = random.sample(
+            north_spawn_area, self.cfg.agent.seller.num // 2
+        ) + random.sample(south_spawn_area, self.cfg.agent.seller.num // 2)
+        random.shuffle(stonecutters_spawn_locations)
+
+        for woodcutter, woodcutter_location in zip(
+            self.woodcutters, woodcutters_spawn_locations
+        ):
+            self.add(woodcutter_location, woodcutter)
+        for stonecutter, stonecutter_location in zip(
+            self.stonecutters, stonecutters_spawn_locations
+        ):
+            self.add(stonecutter_location, stonecutter)
+
+        # NOTE: for now we are only placing a single market (markets[0]) in the middle of the map
+        #       regardless of how many markets there are.
+        self.add((25, 25, 2), self.markets[0])
+
+    def reset(self):
+        """Reset the environment and all its agents."""
+        self.create_world()
+        self.populate()
+        for woodcutter in self.woodcutters:
+            woodcutter.reset()
+        for stonecutter in self.stonecutters:
+            stonecutter.reset()
+        for market in self.markets:
+            market.reset()
---
+++ examples/ai_econ/main.py
@@ -0,0 +1,80 @@
+# general imports
+import argparse
+
+# agentarium imports
+from agentarium.config import load_config
+from agentarium.utils.visualization import (animate, image_from_array,
+                                            visual_field_sprite)
+# imports from our example
+from examples.ai_econ.env import EconEnv
+from examples.ai_econ.utils import create_agents, create_models
+
+
+def setup(cfg) -> EconEnv:
+    """Set up all the whole environment and everything within."""
+
+    # make the agents
+    woodcutter_models, stonecutter_models, market_models = create_models(cfg)
+    woodcutters, stonecutters, markets = create_agents(
+        cfg, woodcutter_models, stonecutter_models, market_models
+    )
+
+    # make the environment
+    return EconEnv(cfg, woodcutters, stonecutters, markets)
+
+
+def run(env: EconEnv, cfg):
+    """Run the experiment."""
+    imgs = []
+    total_seller_score = 0
+    total_seller_loss = 0
+    total_buyer_loss = 0
+    for epoch in range(cfg.experiment.epochs + 1):
+        # Reset the environment at the start of each epoch
+        env.reset()
+        for i in range(cfg.agent.seller.num):
+            env.woodcutters[i].model.start_epoch_action(**locals())
+            env.stonecutters[i].model.start_epoch_action(**locals())
+        for i in range(cfg.agent.buyer.num):
+            env.markets[i].model.start_epoch_action(**locals())
+
+        while not env.turn >= env.max_turns:
+            if epoch % cfg.experiment.record_period == 0:
+                full_sprite = visual_field_sprite(env)
+                imgs.append(image_from_array(full_sprite))
+
+            env.take_turn()
+
+        # At the end of each epoch, train as long as the batch size is large enough.
+        if epoch > 10:
+            for i in range(cfg.agent.seller.num):
+                total_seller_loss += env.woodcutters[i].model.train_step()
+                total_seller_loss += env.stonecutters[i].model.train_step()
+            for i in range(cfg.agent.buyer.num):
+                total_buyer_loss += env.markets[i].model.train_step()
+
+        total_seller_score += env.seller_score
+        current_seller_epsilon = env.woodcutters[0].model.epsilon
+
+        if epoch % cfg.experiment.record_period == 0:
+            avg_seller_score = total_seller_score / cfg.experiment.record_period
+            print(
+                f"Epoch: {epoch}; Epsilon: {current_seller_epsilon}; Losses this period: {total_seller_loss}; Avg. score this period: {avg_seller_score}"
+            )
+            animate(imgs, f"econ_epoch{epoch}", "./data/")
+            # reset the data
+            imgs = []
+            total_score = 0
+            total_loss = 0
+
+        # update epsilon
+        for i in range(cfg.agent.seller.num):
+            new_epsilon = current_seller_epsilon - cfg.experiment.seller_epsilon_decay
+            env.woodcutters[i].model.epsilon = max(new_epsilon, 0.01)
+            env.stonecutters[i].model.epsilon = max(new_epsilon, 0.01)
+
+
+if __name__ == "__main__":
+    config = load_config(argparse.Namespace(config="./configs/config.yaml"))
+    environment = setup(config)
+    run(environment, config)
