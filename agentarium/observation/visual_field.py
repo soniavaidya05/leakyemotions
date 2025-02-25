91c4d7d7f6c00a09b51a7ef433e89951b8a9575c
YBthree <144300516+bing-j@users.noreply.github.com>
2025-02-25 16:14:44 -0500

    debugged visual_field, and implemented visual_field_ascii. Updated docstrings for both. Added option to choose the entity to fill with.

--- agentarium/observation/visual_field.py
+++ agentarium/observation/visual_field.py
@@ -11,6 +11,7 @@ def visual_field(
     entity_map: dict[str, list[float]],
     vision: int | None = None,
     location: tuple | None = None,
+    fill_entity_kind: str = "Wall",
 ) -> np.ndarray:
     """
     Visualize the world.
@@ -20,20 +21,18 @@ def visual_field(
         entity_map (dict[str, list[float]]): The mapping
         between objects and visual appearance.
         vision (Optional, int): The agent's visual field
-        radius. If none, the entire environment.
+        radius. If None, the entire environment. Defaults to None.
         location: (Optional, tuple): The location to centre
-        the visual field on. If none, the entire environment.
+        the visual field on. If None, the entire environment. Defaults to None.
+        fill_entity_kind (Optional, str): if the agent's vision is out of bounds,
+        fill the space with appearances of this entity. Defaults to "Wall".
 
     Returns:
-        np.ndarray: An array of shape
-        `(2 * vision + 1, 2 * vision + 1, env.layers)`.
+        np.ndarray: An array with dtype float64 of shape
+        `(number of channels, 2 * vision + 1, 2 * vision + 1)`.
         Or if vision is None:
-        `(env.height, env.width, env.layers)`.
-
-    Notes:
-        This function squeezes the shape of the world, so if the world has only one layer,
-        the function will return an array of shape `(env.height, env.width)`.
-        TODO: check if this behaviour is correct.
+        `(number of channels, env.width, env.layers)`.
+        Here, the number channels is determined based on the one-hot entity map provided.
     """
     # Get the number of channels used by the model.
     num_channels = len(list(entity_map.values())[0])
@@ -42,22 +41,14 @@ def visual_field(
     new = np.stack(
         [np.zeros_like(env.world, dtype=np.float64) for _ in range(num_channels)],
         axis=0,
-    ).squeeze()
+    )
 
     # Iterate through the world and assign the appearance of the object at that location
-    for layer in range(env.world.shape[-1]):
-        for index, _ in np.ndenumerate(env.world[:, :, layer]):
-            H, W = index  # Get the coordinates
-            # Return visualization image
-            if env.world.shape[-1] > 1:
-                new[:, H, W, layer] = entity_map[env.world[H, W, layer].kind]
-            else:
-                new[:, H, W] = entity_map[env.world[H, W, layer].kind]
-    else:
-        # TODO: does this mean that new is of shape (channels, env.height, env.width)
-        # instead of (env.height, env.width, env.layers)?
-        # what if the world only has 1 layer? Wouldn't this line make it (channels, env.height) instead?
-        new = np.sum(new, axis=-1)
+    for index, x in np.ndenumerate(env.world):
+        # Return visualization image
+        new[:, index] = entity_map[x.kind]
+    # sum the one-hot code over the layers
+    new = np.sum(new, axis=-1)
 
     # If no location, return the full visual field
     if location is None:
@@ -96,12 +87,11 @@ def visual_field(
         new = new[:, slice(*crop_h), slice(*crop_w)]
 
         for index, x in np.ndenumerate(new):
-            C, H, W = index  # Get the coordinates
-            if np.isnan(
-                x
-            ):  # If the current location was outside the shift coordinates...
-                # ...replace the appearance with the wall appearance in this channel.
-                new[index] = entity_map["Wall"][C]
+            C, H, W = index
+            # If the current location was outside the shift coordinates...
+            if np.isnan(x):
+                # ...replace the appearance with the appearance of the fill entity in this channel.
+                new[index] = entity_map[fill_entity_kind][C]
         # Return the agent's sliced observation space
         new = new.astype(np.float64)
         # ==rotate==#
@@ -116,36 +106,49 @@ def visual_field_ascii(
     entity_map: dict[str, str],
     vision: int | None = None,
     location: tuple | None = None,
+    fill_entity_kind: str = "Wall",
 ) -> np.ndarray:
     """
-    Visualize the world with an ascii .
+    Visualize the world with ascii appearances.
+    If the world has multiple layers,
+    and there are multiple non-empty entities on different layers at the same horizontal coordinate,
+    only the top (i.e. highest layer) non-empty entity at that coordinate will be visualized.
 
     Parameters:
         env (GridworldEnv): The environment to visualize.
-        entity_map (dict[str, list[float]]): The mapping
+        entity_map (dict[str, str]): The mapping
         between objects and visual appearance, where the visual appearance must be a character.
         vision (Optional, int): The agent's visual field
-        radius. If none, the entire environment.
+        radius. If None, the entire environment. Defaults to None.
         location: (Optional, tuple): The location to centre
-        the visual field on. If none, the entire environment.
+        the visual field on. If None, the entire environment. Defaults to None.
+        fill_entity_kind (Optional, str): if the agent's vision is out of bounds,
+        fill the space with appearances of this entity. Defaults to "Wall".
 
     Returns:
         np.ndarray: An array of strings of shape
-        `(2 * vision + 1, 2 * vision + 1, env.layers)`.
+        `(2 * vision + 1, 2 * vision + 1)`.
         Or if vision is None:
-        `(env.height, env.width, env.layers)`.
-
-    Notes:
-        Unlike the `visual_field` function, this function does not squeeze the shape of the world.
-        TODO: check if this behaviour is correct.
+        `(env.height, env.width)`.
     """
 
     # Create an array of equivalent shape to the world map
     new = np.empty_like(env.world, dtype=np.str_)
 
+    # TODO: instead, "stack" the layers and if there are multiple objects on top, only return the top one.
+    # TODO: empty entities should be a period or an underscore
     # Iterate through the world and assign the appearance of the object at that location
-    for index, _ in np.ndenumerate(env.world):
-        new[index] = entity_map[env.world[index].kind]
+    for index, _ in np.ndenumerate(env.world[:, :, 0]):
+        H, W = index
+        # iterate from top to bottom
+        for L in reversed(range(env.world.shape[2])):
+            # if the entity is not empty, get its appearance, and we don't need to check the lower layers.
+            if env.world[H, W, L].kind != "EmptyEntity":
+                new[H, W] = entity_map[env.world[H, W, L].kind]
+                break
+            # continue to check the lower layers if the entity is not empty.
+            else:
+                new[H, W] = entity_map[env.world[H, W, L].kind]
 
     # If no location, return the full visual field
     if location is None:
@@ -156,17 +159,12 @@ def visual_field_ascii(
         # The centrepoint for the shift array is defined by the centrepoint on the main array
         # E.g. the centrepoint for a 9x9 array is (4, 4). So, the shift array for the location
         # (1, 6) is (3, -2): left three, up two.
-        shift_dims = np.hstack(
-            np.subtract(
-                [env.world.shape[0] // 2, env.world.shape[1] // 2], location[0:2]
-            ),
-            # we do not need to shift the layer.
-            [0],
+        shift_dims = np.subtract(
+            [env.world.shape[0] // 2, env.world.shape[1] // 2], location[0:2]
         )
 
-        # Shift the array.
-        # Replace coordinates outside the map with nans. We will fix this later.
-        new = shift(array=new, shift=shift_dims, cval=np.nan)
+        # Shift the array, and fill the appearances of coordinates outside the map with the fill entity's appearance.
+        new = shift(array=new, shift=shift_dims, cval=entity_map[fill_entity_kind])
 
         # Set up the dimensions of the array to crop
         crop_h = (
@@ -178,14 +176,7 @@ def visual_field_ascii(
             env.world.shape[1] // 2 + vision + 1,
         )
         # Crop the array to the selected dimensions
-        new = new[slice(*crop_h), slice(*crop_w), :]
+        new = new[slice(*crop_h), slice(*crop_w)]
 
-        for index, x in np.ndenumerate(new):
-            H, W, L = index  # Get the coordinates
-            if np.isnan(
-                x
-            ):  # If the current location was outside the shift coordinates...
-                # ...replace the appearance with the wall appearance in this channel.
-                new[index] = entity_map["Wall"]
         # Return the agent's sliced observation space
         return new.astype(np.str_)
