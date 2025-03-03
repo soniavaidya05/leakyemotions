79b4ae829c4b92a8dc7af6f2adb31e4d904722df
YBthree <144300516+bing-j@users.noreply.github.com>
2025-03-03 17:14:14 -0500

    added math extension for sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ release = "0.1.0"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_nb", "sphinx.ext.autodoc"]
+extensions = ["myst_nb", "sphinx.ext.autodoc", "sphinx.ext.pngmath"]
 
 templates_path = ["_templates"]
 exclude_patterns = []
